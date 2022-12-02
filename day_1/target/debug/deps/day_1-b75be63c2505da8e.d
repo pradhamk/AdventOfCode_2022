2fe9fe52264c106e792606a3afc16e742ba196b6
pradhamk <pradhamk@pradhamk.com>
2022-12-02 16:44:59 -0500

    remove target dirs

--- day_1/target/debug/deps/day_1-b75be63c2505da8e.d
+++
@@ -1,5 +0,0 @@
-/home/pradhamk/Coding/Github/AdventOfCode_2022/day_1/target/debug/deps/day_1-b75be63c2505da8e.rmeta: src/main.rs
-
-/home/pradhamk/Coding/Github/AdventOfCode_2022/day_1/target/debug/deps/day_1-b75be63c2505da8e.d: src/main.rs
-
-src/main.rs:
